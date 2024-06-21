d5e74895769528516b1f570c0726cad447785c96
Gabriele Cimato <gabriele.cimato+github@gmail.com>
2024-06-20 23:06:08 -0500

    docs: add comments explainig some basic rust syntax

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,18 @@ const DEFAULT_ADDRESS_AND_PORT: &str = "127.0.0.1:8080";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // When I use the await it means I want to wait this asynchronous request
+    // to complete before moving forward. The ? indicates that it can fail and
+    // it will return the error to the caller, so it bubbles that up.
+    // This is possible if the function this is in has a return value matching
+    // the error returned potentially by this async call.
     let listener = TcpListener::bind(DEFAULT_ADDRESS_AND_PORT).await?;
 
     loop {
+        // Here I have a tuple returned from listener.accept, the first value
+        // is declared as mutable. While I wasn't sure why this is necessary here
+        // it seems that the next calls to read/write_all expect the socket to be
+        // mutable (TODO: will have to dig more on this)
         let (mut socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
